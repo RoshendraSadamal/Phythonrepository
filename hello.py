e35a23a92bd8a8b9e3e98217f118088c2c41cd33
Roshendra Sadamal <34857564+RoshendraSadamal@users.noreply.github.com>
2024-12-25 18:52:04 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,11 +3,11 @@
 print("Hello, World!")
 
 
-def add_numbers(a, b):
+def add(a, b):
     return a + b
 
 
-result = add_numbers(5, 3)
+resultadd = add(5, 3)
 
 
-print(f"The result of adding 5 and 3 is: {result}")
+print(f"The result of adding 5 and 3 is: {resultadd}")
